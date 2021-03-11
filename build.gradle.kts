2902825d0713b32943b7879dcb918b5a7154145d
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-11 21:17:22 +0900

    #1 [CHORE] : build.gradle 파일 kotlin dsl로 바꾸기

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = "1.4.31"
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.1.2"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-
+        classpath("com.android.tools.build:gradle:${Versions.gradle_version}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin_version}")
+        classpath(ClassPathPlugins.hilt)
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -21,6 +20,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,20 @@
+import org.gradle.api.JavaVersion
+
+object Versions {
+    const val kotlin_version = "1.4.31"
+    const val core_ktx_version = "1.3.2"
+    const val appcompat_version = "1.2.0"
+    const val material_design_version = "1.3.0"
+    const val constraint_layout_version = "2.0.4"
+    const val hilt_version = "2.33-beta"
+    const val nav_verion = "2.3.3"
+
+    const val gradle_version = "4.1.2"
+
+    const val junit_version = "4.13.2"
+    const val espresso_version = "3.3.0"
+    const val androidtest_version = "1.1.2"
+
+    val java_version = JavaVersion.VERSION_1_8
+    const val jvm_version = "1.8"
+}
