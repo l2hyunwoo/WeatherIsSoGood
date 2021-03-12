5ee54c0a2fb3d54de05f606d24f9b70f26fee7c5
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-12 20:34:20 +0900

    #9 [CHORE] : JSON 파일 로드 리스트 만들기
    - City Item xml 만들기
    - List가 들어갈 Activity에 RecyclerView 만들기

--- app/src/main/java/co/spoonradio/recruit/presentation/search/CityInfoActivity.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/search/CityInfoActivity.kt
@@ -1,6 +1,7 @@
 package co.spoonradio.recruit.presentation.search
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import co.spoonradio.recruit.R
 import co.spoonradio.recruit.base.BindingActivity
 import co.spoonradio.recruit.databinding.ActivityCityInfoBinding
@@ -8,6 +9,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CityInfoActivity : BindingActivity<ActivityCityInfoBinding>(R.layout.activity_city_info) {
+    private val cityViewModel: CityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
