ed7fad2e373f268e75eb8b296c9d0f1677c58dc5
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-12 21:55:25 +0900

    #9 [CHORE] : JSON 파일 로드 리스트 만들기
    - CityAdapter 연결
    - LiveData 옵저빙

--- app/src/main/java/co/spoonradio/recruit/presentation/city/CityInfoActivity.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/city/CityInfoActivity.kt
@@ -1,16 +1,39 @@
 package co.spoonradio.recruit.presentation.city
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import co.spoonradio.recruit.R
 import co.spoonradio.recruit.base.BindingActivity
 import co.spoonradio.recruit.databinding.ActivityCityInfoBinding
+import co.spoonradio.recruit.presentation.city.adapter.CityAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CityInfoActivity : BindingActivity<ActivityCityInfoBinding>(R.layout.activity_city_info) {
     private val cityViewModel: CityViewModel by viewModels()
+    private val cityAdapter: CityAdapter by lazy { CityAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.lifecycleOwner = this
+
+        setAdapter()
+        subscribeData()
+    }
+
+    private fun subscribeData() {
+        with(cityViewModel) {
+            Log.d("TAG", "Here sub")
+            cityList.observe(this@CityInfoActivity) {
+                Log.d("TAG", it.toString())
+                cityAdapter.submitList(it)
+            }
+        }
+    }
+
+    private fun setAdapter() {
+        binding.rvCityList.adapter = cityAdapter
+        cityViewModel.getCityInfo()
     }
 }
