0fdaa8eebe1ce2443736e278b22a66cc2ab48adb
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-13 00:26:00 +0900

    #10 [FEAT] : API를 사용해 현재 날씨를 조회
    - WeatherViewModel.kt 만들기

--- app/src/main/java/co/spoonradio/recruit/presentation/detail/DetailActivity.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/detail/DetailActivity.kt
@@ -1,15 +1,19 @@
 package co.spoonradio.recruit.presentation.detail
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.viewModels
 import co.spoonradio.recruit.R
 import co.spoonradio.recruit.base.BindingActivity
 import co.spoonradio.recruit.databinding.ActivityDetailBinding
+import co.spoonradio.recruit.util.StatusBarUtil
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailActivity : BindingActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+    private val weatherViewModel: WeatherViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StatusBarUtil.setStatusBar(this, Color.TRANSPARENT, false)
     }
 }
