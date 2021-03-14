e704cb19abdb76d16fbc95d863d305ba2f124058
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-15 02:05:34 +0900

    #15 [FEAT] : Instant Search 구현

--- app/src/main/java/co/spoonradio/recruit/presentation/city/CityViewModel.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/city/CityViewModel.kt
@@ -1,14 +1,23 @@
 package co.spoonradio.recruit.presentation.city
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.util.Log
+import androidx.lifecycle.*
 import co.spoonradio.recruit.data.entity.City
 import co.spoonradio.recruit.data.repository.CityRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
+
+sealed class SearchResult
+class ValidResult(val result: List<City>) : SearchResult()
+object Empty : SearchResult()
+object EmptyQuery : SearchResult()
+class Error(val error: Throwable) : SearchResult()
+object TerminalError : SearchResult()
+
 
 @HiltViewModel
 class CityViewModel @Inject constructor(
@@ -17,6 +26,23 @@ class CityViewModel @Inject constructor(
     private val _cityList = MutableLiveData<List<City>>()
     val cityList: LiveData<List<City>>
         get() = _cityList
+
+    @ExperimentalCoroutinesApi
+    val queryChannel = BroadcastChannel<String>(Channel.CONFLATED)
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    val searchResult = queryChannel.asFlow()
+        .debounce(500L)
+        .mapLatest {
+            if(it.isEmpty())
+                EmptyQuery
+            else {
+                val result = withContext(Dispatchers.IO) {cityRepository.searchCities(it) }
+                if(result.isNotEmpty()) ValidResult(result)
+                else Empty
+            }
+        }.asLiveData()
 
     fun getCityInfo() {
         viewModelScope.launch {
--- app/src/main/java/co/spoonradio/recruit/presentation/city/WeatherSearchFragment.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/city/WeatherSearchFragment.kt
@@ -1,21 +1,84 @@
 package co.spoonradio.recruit.presentation.city
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import co.spoonradio.recruit.R
 import co.spoonradio.recruit.base.BindingFragment
+import co.spoonradio.recruit.data.entity.City
 import co.spoonradio.recruit.databinding.FragmentWeatherSearchBinding
+import co.spoonradio.recruit.presentation.city.adapter.CityAdapter
+import co.spoonradio.recruit.presentation.detail.DetailActivity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.launch
 
-class WeatherSearchFragment : BindingFragment<FragmentWeatherSearchBinding>(R.layout.fragment_weather_search) {
+class WeatherSearchFragment :
+    BindingFragment<FragmentWeatherSearchBinding>(R.layout.fragment_weather_search) {
+    private val cityAdapter: CityAdapter by lazy { CityAdapter(provideItemClickListener()) }
+    private val cityViewModel: CityViewModel by activityViewModels()
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initView()
         return binding.root
+    }
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    private fun initView() {
+        with(binding) {
+            rvSearchResult.adapter = cityAdapter
+            etSearchQuery.doAfterTextChanged {
+                lifecycleScope.launch { cityViewModel.queryChannel.send(it.toString()) }
+            }
+        }
+        cityViewModel.searchResult.observe(viewLifecycleOwner) { handleSearchResult(it) }
+    }
+
+    private fun handleSearchResult(searchResult: SearchResult) {
+        when (searchResult) {
+            is ValidResult -> {
+                with(binding) {
+                    rvSearchResult.visibility = View.VISIBLE
+                    txtSearchWarning.visibility = View.GONE
+                }
+                cityAdapter.submitList(searchResult.result)
+            }
+            is Empty -> {
+                with(binding) {
+                    rvSearchResult.visibility = View.GONE
+                    txtSearchWarning.visibility = View.VISIBLE
+                }
+            }
+            else -> {
+                with(binding) {
+                    rvSearchResult.visibility = View.GONE
+                    txtSearchWarning.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    private fun provideItemClickListener(): CityAdapter.OnClickListener {
+        return object : CityAdapter.OnClickListener {
+            override fun itemClickListener(city: City) {
+                val intent = Intent(requireActivity(), DetailActivity::class.java)
+                intent.putExtra("city", city)
+                startActivity(intent)
+            }
+        }
     }
 }
