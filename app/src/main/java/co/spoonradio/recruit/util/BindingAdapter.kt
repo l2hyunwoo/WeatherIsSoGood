c332485d8643c996851f8db274a11a3ce05b2a35
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-14 13:00:19 +0900

    #13 [FEAT] : Detail - 날씨 화면 설계
    - Detail 화면 설계

--- app/src/main/java/co/spoonradio/recruit/util/BindingAdapter.kt
+++ app/src/main/java/co/spoonradio/recruit/util/BindingAdapter.kt
@@ -1,11 +1,28 @@
 package co.spoonradio.recruit.util
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
+const val KELVIN_CONSTANT = 273.15
+
 @BindingAdapter("app:srcFromUrl")
 fun ImageView.setSrcFromUrl(url: String) {
+    Glide.with(this.context)
+        .load(url)
+        .into(this)
+}
+
+@BindingAdapter("app:setCelsiusTemperature")
+fun TextView.setCelsiusTemperature(temperature: Double) {
+    val temperatureString = "${temperature - KELVIN_CONSTANT}°C"
+    this.text = temperatureString
+}
+
+@BindingAdapter("app:setWeatherIcon")
+fun ImageView.setWeatherIcon(icon: String) {
+    val url = "http://openweathermap.org/img/wn/${icon}@2x.png"
     Glide.with(this.context)
         .load(url)
         .into(this)
