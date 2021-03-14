75267458b3f93d2b4472838c5c84c99c5571a319
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-14 18:01:16 +0900

    #15 [FEAT] : 리스트 CityListFragment.kt로 옮기기

--- app/src/main/java/co/spoonradio/recruit/presentation/city/CityListFragment.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/city/CityListFragment.kt
@@ -1,20 +1,52 @@
 package co.spoonradio.recruit.presentation.city
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import co.spoonradio.recruit.R
 import co.spoonradio.recruit.base.BindingFragment
+import co.spoonradio.recruit.data.entity.City
 import co.spoonradio.recruit.databinding.FragmentCityListBinding
+import co.spoonradio.recruit.presentation.city.adapter.CityAdapter
+import co.spoonradio.recruit.presentation.detail.DetailActivity
 
 class CityListFragment : BindingFragment<FragmentCityListBinding>(R.layout.fragment_city_list) {
+    private val cityViewModel: CityViewModel by activityViewModels()
+    private val cityAdapter: CityAdapter by lazy { CityAdapter(provideItemClickListener()) }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        setAdapter()
+        subscribeData()
         return binding.root
+    }
+
+    private fun subscribeData() {
+        with(cityViewModel) {
+            cityList.observe(viewLifecycleOwner) { cityAdapter.submitList(it) }
+        }
+    }
+
+    private fun setAdapter() {
+        binding.rvCityList.adapter = cityAdapter
+        cityViewModel.getCityInfo()
+    }
+
+    private fun provideItemClickListener(): CityAdapter.OnClickListener {
+        return object : CityAdapter.OnClickListener {
+            override fun itemClickListener(city: City) {
+                val intent = Intent(requireActivity(), DetailActivity::class.java)
+                intent.putExtra("city", city)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/co/spoonradio/recruit/presentation/city/adapter/MenuPagerAdapter.kt
+++ app/src/main/java/co/spoonradio/recruit/presentation/city/adapter/MenuPagerAdapter.kt
@@ -1,15 +1,17 @@
 package co.spoonradio.recruit.presentation.city.adapter
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import co.spoonradio.recruit.presentation.city.CityListFragment
 import co.spoonradio.recruit.presentation.city.WeatherSearchFragment
 
-class MenuPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+class MenuPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount() = 2
 
     override fun createFragment(position: Int): Fragment {
-        return when(position) {
+        return when (position) {
             0 -> CityListFragment()
             1 -> WeatherSearchFragment()
             else -> throw IllegalArgumentException("Wrong Position $position")
