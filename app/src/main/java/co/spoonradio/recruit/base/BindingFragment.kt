edf5060d756f6b601a4fdd767611887dc4a5ddc4
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-14 14:41:52 +0900

    #15 [FEAT] : 검색 메뉴 구성
    - BindingFragment.kt 설정

---
+++ app/src/main/java/co/spoonradio/recruit/base/BindingFragment.kt
@@ -0,0 +1,34 @@
+package co.spoonradio.recruit.base
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+abstract class BindingFragment<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
